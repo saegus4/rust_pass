6ab4309a355051de4cc57b849f9880b4afc32225
Francisco Leite <franciscoleite.dev@protonmail.com>
2025-07-25 23:07:27 +0000

    Make it possible to decrypt gpg files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, path::PathBuf};
+use std::{env, fs, path::PathBuf, process::Command};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,8 +10,19 @@ fn main() {
         .join(".password-store")
         .join(format!("{name}.gpg"));
 
-    match fs::metadata(file_path) {
-        Ok(_) => println!("Going"),
+    match fs::metadata(&file_path) {
+        Ok(_) => get_password_value(file_path),
         Err(_) => println!("Tu-tu-ru")
     }
+}
+
+fn get_password_value(file_path: PathBuf) {
+    let output = Command::new("gpg")
+        .arg("--batch")
+        .arg("--yes")
+        .arg("--decrypt")
+        .arg(&file_path)
+        .output().unwrap();
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
 }
