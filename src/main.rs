bab5de4b435abdb7a326b8a06c8849d331f0b8d5
Francisco Leite <franciscoleite.dev@protonmail.com>
2025-08-02 18:56:41 +0000

    Add insert command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::{self, File}, io::Write, path::{Path, PathBuf}, process::Command};
+use std::{env, fs::{self, File}, io::{self, Write}, path::{Path, PathBuf}, process::Command};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,6 +13,33 @@ fn main() {
 }
 
 fn insert_password(args: Vec<String>) {
+    let name = &args[3];
+    let password_prompt = format!("Enter password for {}:", name);
+    let password = rpassword::prompt_password(password_prompt).unwrap();
+    let second_password_prompt = format!("Retype password for {}:", name);
+    let second_password = rpassword::prompt_password(second_password_prompt).unwrap();
+
+    if !(password == second_password) {
+        println!("Error: the entered passwords do not match.");
+    }
+
+    let mut temp_password_file = File::create("passwords/temp").expect("Failed to create temp file");
+    temp_password_file.write_all(&password.into_bytes()).expect("Failed to write temp file");
+
+    let key = fs::read_to_string("passwords/.gpg-id").expect("Failed to read gpg id file").trim().to_owned();
+    let password_file_name = format!("passwords/{}.gpg", name);
+    let output = Command::new("gpg")
+        .arg("--batch")
+        .arg("--yes")
+        .arg("--encrypt")
+        .arg("--recipient")
+        .arg(&key)
+        .arg("--output")
+        .arg(&password_file_name)
+        .arg("passwords/temp")
+        .output().unwrap();
+
+    fs::remove_file("passwords/temp").expect("Failed to remove temp file");
 }
 
 fn init_pass_vault(args: Vec<String>) {
@@ -23,8 +50,8 @@ fn init_pass_vault(args: Vec<String>) {
 
     let gpg_key = Command::new("sh")
         .arg("-c")
-        .arg(r#"gpg --list-secret-keys --keyid-format LONG \
-            | awk '/^sec/ {split($2,a,"/"); print a[2]}'"#)
+        .arg(r#"gpg --list-secret-keys --with-colons --fingerprint \
+            | awk -F: '/^fpr:/ {print $10; exit}'"#)
         .output()
         .expect("failed to get default gpg key");
 
